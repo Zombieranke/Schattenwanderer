c6a697479195ecfd9487ce75fae01921ce58d04a
Xiromtz <moritz@fog.co.at>
2014-11-14 15:12:23 +0100

    New Game Menu-Option implemented

--- src/me/zombieranke/Schatttenwanderer/GameMenu.java
+++ src/me/zombieranke/Schatttenwanderer/GameMenu.java
@@ -2,40 +2,46 @@ package me.zombieranke.Schatttenwanderer;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.gui.MouseOverArea;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class GameMenu extends BasicGameState {
 	
+	//Image Deklarations
+	private Image background;
+	
+	// Class Objects
+	private MenuFunc NewGame;
+	private MenuFunc Levels;
+	private MenuFunc Options;
+	private MenuFunc Exit;
+	
 	private int ID = 1;
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
-		// TODO Auto-generated method stub
-		
+		background = new Image("res/Background.jpg");
+		NewGame = new MenuFunc(new MouseOverArea(container, new Image("res/New_Game_Unlighted.png"), 130, 150), new Image("res/New_Game_Lighted.png"));
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
-		g.drawString("Hier koennte ihr Menu entstehen", 100, 100);
-		g.drawString("Press 2 to enter game", 100, 115);
-		
+		background.draw();
+		NewGame.draw(g, container);
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta)
 			throws SlickException {
+		NewGame.update(delta);
 		Input input = container.getInput();
-		if(input.isKeyPressed(Input.KEY_2)){
+		if(input.isMousePressed(Input.MOUSE_LEFT_BUTTON) && NewGame.IsMouseOver()){
 			game.enterState(2);
 		}
 		if(input.isKeyPressed(Input.KEY_ESCAPE)){
