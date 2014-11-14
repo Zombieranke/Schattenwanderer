30fcd51fa122fed6677ceef3f457b838f6a2c412
Xiromtz <moritz@fog.co.at>
2014-11-14 15:13:51 +0100

    Menu Funktion

---
+++ src/me/zombieranke/Schatttenwanderer/MenuFunc.java
@@ -0,0 +1,31 @@
+package me.zombieranke.Schatttenwanderer;
+import org.newdawn.slick.*;
+import org.newdawn.slick.gui.MouseOverArea;
+
+public class MenuFunc {
+
+	private MouseOverArea area; // Create a Mouseover area
+	private Image image; // Create an image to switch the MouseOverArea
+	
+	// Constructors
+	public MenuFunc(MouseOverArea area, Image image) {
+		this.area = area;
+		this.image = image;
+	}
+	
+	public void draw(Graphics g, GameContainer container)
+	{
+		area.render(container, g);
+	}
+	
+	public void update (int delta)
+	{
+		area.setMouseOverImage(image);
+	}
+	
+	public boolean IsMouseOver()
+	{
+		return area.isMouseOver();
+	}
+
+}
