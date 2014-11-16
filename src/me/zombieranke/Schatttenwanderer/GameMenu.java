3a355e4a1f753ac47e10b56815be9b37f0315c9d
Xiromtz <moritz@fog.co.at>
2014-11-16 22:35:00 +0100

    Full Menu

--- src/me/zombieranke/Schatttenwanderer/GameMenu.java
+++ src/me/zombieranke/Schatttenwanderer/GameMenu.java
@@ -27,24 +27,51 @@ public class GameMenu extends BasicGameState
 	public void init(GameContainer container, StateBasedGame game) throws SlickException
 	{
 		background = new Image("res/Background.jpg");
-		NewGame = new MenuFunc(new MouseOverArea(container, new Image("res/New_Game_Unlighted.png"), 130, 150), new Image("res/New_Game_Lighted.png"));
+		
+		// Initialisiere die Menüpunkte als MouseOverArea und gib ihnen ein Standard und ein mouseover image
+		NewGame = new MenuFunc(new MouseOverArea(container, new Image("res/New_Game_Unlighted.png"), 
+															130, 110), 
+															new Image("res/New_Game_Lighted.png"));
+		Levels = new MenuFunc(new MouseOverArea(container, new Image("res/Levels_Unlighted.png"), 
+														   300, 300), 
+														   new Image("res/Levels_Lighted.png"));
+		Options = new MenuFunc(new MouseOverArea(container, new Image("res/Options_Unlighted.png"), 
+															230, 480), 
+															new Image("res/Options_Lighted.png"));
+		Exit = new MenuFunc(new MouseOverArea(container, new Image("res/Exit_Unlighted.png"),
+														 450, 660),
+														 new Image("res/Exit_Lighted.png"));													
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException
 	{
 		background.draw();
+		
+		// Draw Menu points
 		NewGame.draw(g, container);
+		Levels.draw(g, container);
+		Options.draw(g, container);
+		Exit.draw(g, container);
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException
 	{
+		// Update option points to react to mouseovers
 		NewGame.update(delta);
+		Levels.update(delta);
+		Options.update(delta);
+		Exit.update(delta);
+		
 		Input input = container.getInput();
-		if(input.isMousePressed(Input.MOUSE_LEFT_BUTTON) && NewGame.IsMouseOver())
+		if(input.isMousePressed(Input.MOUSE_LEFT_BUTTON) && NewGame.IsMouseOver()) // If Mouse is pressed and mouse is over the mouseoverarea
 		{
 			game.enterState(2);
+		}
+		if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON) && Exit.IsMouseOver())
+		{
+			container.exit();
 		}
 		if(input.isKeyPressed(Input.KEY_ESCAPE))
 		{
