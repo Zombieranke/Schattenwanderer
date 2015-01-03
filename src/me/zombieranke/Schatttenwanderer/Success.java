aa659c1537b5fd4001187f7e627e098e95214f33
Xiromtz <moritz@fog.co.at>
2015-01-03 01:00:56 +0100

    Game Won Screen Updated! Animation ist drinnen!

--- src/me/zombieranke/Schatttenwanderer/Success.java
+++ src/me/zombieranke/Schatttenwanderer/Success.java
@@ -2,6 +2,7 @@ package me.zombieranke.Schatttenwanderer;
 
 import me.zombieranke.utils.Ressources;
 
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -13,27 +14,37 @@ import org.newdawn.slick.state.StateBasedGame;
 public class Success extends BasicGameState
 {
 	private Image background;
+	private Animation blood;
 	private int ID = 2;
 	
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException
 	{
 		background = Ressources.BACKGROUND_SUCCESS;
+		blood = new Animation(Ressources.GAME_WON_SPRITESHEET, 100);
 	}
 	
 	@Override
 	public void render(GameContainer container,StateBasedGame game, Graphics g) throws SlickException
 	{
 		background.draw();
-		
+		blood.draw(478, 780);
 	}
 	
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException
 	{
+		blood.update(delta);
+		blood.start();
+		if(blood.getFrame() == 48)
+		{
+			blood.setCurrentFrame(25);
+		}
 		Input input = container.getInput();
 		if(input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON) || input.isKeyPressed(Input.KEY_ESCAPE))
 		{
+			blood.setCurrentFrame(0);
+			blood.stop();
 			game.enterState(1);
 		}
 	}
