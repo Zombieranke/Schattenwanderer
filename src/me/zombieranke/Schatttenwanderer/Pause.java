54c037b638613c2a743c8d7b07aec6cf989837be
Zelda Fan <zeldafan42@gmail.com>
2015-01-11 18:26:59 +0100

    Warnings cleanup

--- src/me/zombieranke/Schatttenwanderer/Pause.java
+++ src/me/zombieranke/Schatttenwanderer/Pause.java
@@ -4,15 +4,11 @@ package me.zombieranke.Schatttenwanderer;
 
 import me.zombieranke.utils.Ressources;
 
-import java.awt.Font;
-
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
